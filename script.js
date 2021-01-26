30be979b38f6377723637b8f0a4eb55cb03fa027
vzm5 <73701054+vzm5@users.noreply.github.com>
2021-01-26 16:12:58 -0500

    Function printMessage is developed.

--- script.js
+++ script.js
@@ -11,14 +11,20 @@ const carGenerator = {
 }
 
 function createCar(){
-    //Generate random color
+    //Generate random details of the car
     const carColor = carGenerator.color[Math.floor(Math.random() * carGenerator.color.length)];
     const carBuild = carGenerator.build[Math.floor(Math.random() * carGenerator.build.length)];
     const carMake = carGenerator.make[Math.floor(Math.random() * carGenerator.make.length)];
-    return carColor, carBuild, carMake;
+
+    const myCar = [carColor, carBuild, carMake];
+    return myCar;
 }
 
 function printMessage(){
-    
+    const myCar = createCar();
+
+    //Print out sentences describing the generated car.
+    console.log("You will be driving a " + myCar[0] + " " + myCar[2] + " " + myCar[1] + ".");
 }
-console.log(createCar());
+
+printMessage();
