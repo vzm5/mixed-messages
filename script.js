e63118c3ba51e33fa2f720369c811c1a1ddd8b8a
vzm5 <73701054+vzm5@users.noreply.github.com>
2021-01-26 16:05:03 -0500

    Function createCar() is created.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Mixed Messages");
+/*Mixed Messages
+script.js 
+This is a random car generating app. It can be used by someone who wants to get new ideas
+for car builds.
+*/
+
+const carGenerator = {
+    color : ["Millenial Pink", "Midnight Black", "Silver", "Baby Blue", "Space Grey"],
+    build : ["sedan", "hot hatch", "convertible", "truck", "SUV"],
+    make : ["Mazda", "Ford", "Ferarri", "Porsche", "Volkswagen"]
+}
+
+function createCar(){
+    //Generate random color
+    const carColor = carGenerator.color[Math.floor(Math.random() * carGenerator.color.length)];
+    const carBuild = carGenerator.build[Math.floor(Math.random() * carGenerator.build.length)];
+    const carMake = carGenerator.make[Math.floor(Math.random() * carGenerator.make.length)];
+    return carColor, carBuild, carMake;
+}
+
+function printMessage(){
+    
+}
+console.log(createCar());
